36e7bab04ba39dfb23f81cdc80094f88bb0af9f8
Manuel Escobar <manuel.escobar1217@gmail.com>
2024-09-12 00:37:23 -0500

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import PokemonGame from '@/modules/pokemon/pages/PokemonGame.vue';
+import FooterApp from '@/modules/components/FooterApp.vue';
 </script>
 
 <template>
   <PokemonGame />
+  <FooterApp />
 </template>
---
+++ src/modules/components/FooterApp.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="w-full fixed bottom-0 left-0 text-center py-2 shadow-inner text-white">
+    <a href="https://github.com/ManuelBlaze" target="”_blank”">
+      <p class="text-sm">Made with <span class="text-red-500">♥</span> by Blaze</p>
+    </a>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  background-color: #114b5f;
+  cursor: pointer;
+}
+</style>
